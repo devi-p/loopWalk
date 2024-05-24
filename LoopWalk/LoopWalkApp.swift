02259dad44128ffc3d67056040d601be43972513
devi-p <ponugupa@ualberta.ca>
2024-05-24 21:44:43 +0400

    Initial Commit

---
+++ LoopWalk/LoopWalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoopWalkApp.swift
+//  LoopWalk
+//
+//  Created by vagdevi ponugupati on 24/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct LoopWalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
